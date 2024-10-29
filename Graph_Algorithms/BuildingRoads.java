9104f0473dab6c5f82bfa18b868e6efe0abc11a4
yakash222000 <yakash222000@gmail.com>
2024-10-29 11:57:38 +0530

    pushed

--- Graph_Algorithms/BuildingRoads.java
+++ Graph_Algorithms/BuildingRoads.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class BuildingRoads {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
